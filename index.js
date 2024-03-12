c6f220b6ec4015fab805794377ae26582ab85a1e
SchneiderNejc <nejc.sch@gmail.com>
2024-03-12 18:05:09 +0100

    create wallet instance

--- index.js
+++ index.js
@@ -5,5 +5,10 @@ const {
     Keypair,
     LAMORTS_PER_SOL
 } = require("@solana/web3.js")
+
+const wallet = new Keypair()
+
+const publicKey = new PublicKey(wallet._keypair.publicKey);
+const secretKey = wallet._keypair.secretKey
 //---------------Funcion Exec----------------------
 main()
