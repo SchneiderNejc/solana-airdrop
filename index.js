0b48716c51ac48601e6bfdc80365e68214f1d006
SchneiderNejc <nejc.sch@gmail.com>
2024-03-12 18:04:58 +0100

    import solana/web3.js modules

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const {
+    Connection,
+    PublicKey,
+    clusterApiUrl,
+    Keypair,
+    LAMORTS_PER_SOL
+} = require("@solana/web3.js")
 //---------------Funcion Exec----------------------
 main()
