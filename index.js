c5fb4595ff9c97d2dde9216b1a358d891b90061d
SchneiderNejc <nejc.sch@gmail.com>
2024-03-12 18:05:27 +0100

    get wallet balance

--- index.js
+++ index.js
@@ -10,5 +10,15 @@ const wallet = new Keypair()
 
 const publicKey = new PublicKey(wallet._keypair.publicKey);
 const secretKey = wallet._keypair.secretKey
+
+const getWalletBalance = async() => {
+    try {
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        const walletBalance = await connection.getBalance(publicKey)
+        console.log(`Wallet balance is ${walletBalance}`)
+    } catch(err) {
+        console.error(err)
+    }
+}
 //---------------Funcion Exec----------------------
 main()
